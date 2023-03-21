4fb3f92fb406cda48b31a23ac414af2d67fa0e01
Artmansky <tomaszartmanski@gmail.com>
2023-03-21 23:49:46 +0100

    Small refactor for imports

--- RandomAnime.py
+++ RandomAnime.py
@@ -1,13 +1,12 @@
 #Imports that are all required for script to work. Random picks random integer. Element Tree handles json responses and makes XML work easy. Requests and webbrowser to communicate with server.
-import random
+import random as RAND
 import xml.etree.ElementTree as ET
-import requests
-import webbrowser
-from tkinter import *
-from tkinter import messagebox
+import requests as REQ
+import webbrowser as WEB
+import tkinter as TK
 
 #Random library to choose random title
-def randomint(min,max): return random.randint(min,max)
+def randomint(min,max): return RAND.randint(min,max)
 
 #Whole method to parse previously downloaded animes from selected site, argument is name of the xml file
 def grabFromFile(filename):
@@ -15,7 +14,7 @@ def grabFromFile(filename):
   try:
     tree = ET.parse(filename)
   except:
-    messagebox.showerror("Error!","Last anime list doesn't exist!")
+    TK.messagebox.showerror("Error!","Last anime list doesn't exist!")
     return
   #Empty lists for titles and their urls on the sites
   titleArray=[]
@@ -24,12 +23,12 @@ def grabFromFile(filename):
   root = tree.getroot()
   #Titles and urls are added to their lists in the same time so they have same position in both lists
   for anime in root.findall('anime'):
-      titleArray.append(anime.find('series_title').text)
-      urls.append(anime.find('my_url').text)
+    titleArray.append(anime.find('series_title').text)
+    urls.append(anime.find('my_url').text)
   #Random title and it's url are choosen and put on the screen for the User
   rand = randomint(0,len(titleArray)-1)
-  msgBox = messagebox.askquestion('Result','Your next anime is: ' + titleArray[rand] + ". Would you like to open anime's site?")
-  if(msgBox) == 'yes': webbrowser.open(urls[rand])
+  msgBox = TK.messagebox.askquestion('Result','Your next anime is: ' + titleArray[rand] + ". Would you like to open anime's site?")
+  if(msgBox) == 'yes': WEB.open(urls[rand])
 
 #Request to AniList api      
 def queryReqAL(name):
@@ -54,7 +53,7 @@ def queryReqAL(name):
     'username': name,
     }
     #Sends post to the server and gets json back then uses Element Tree to handle response
-    response = requests.post('https://graphql.anilist.co', json={'query': query, 'variables': variables})
+    response = REQ.post('https://graphql.anilist.co', json={'query': query, 'variables': variables})
     data_json = response.json()
     root = ET.Element("myanimelist")
     #If json was fine script is searching for animes in json and puts names with urls inside Element Tree. If json is empty shows error message that user doesn't exist in the base
@@ -64,29 +63,29 @@ def queryReqAL(name):
         ET.SubElement(newAnime,"series_title").text = str(anime["media"]["title"]["romaji"])
         ET.SubElement(newAnime,"my_url").text = str(anime["media"]["siteUrl"])
     except:
-      messagebox.showerror("Error!","No user found! Check spelling!")
+      TK.messagebox.showerror("Error!","No user found! Check spelling!")
       return
     #Saves Element Tree to the file and executes method to pick random anime
     save = ET.ElementTree(root)
     save.write("listAL.xml")
     grabFromFile("listAL.xml")
-  if not name: messagebox.showerror("Name not given!","Input field is empty!")
+  if not name: TK.messagebox.showerror("Name not given!","Input field is empty!")
 
 #Request to MAL "api"
 def queryReqMAL(name):
   if name:
     #Tries getting response from MAL site (it's json response). If not shows error that user isn't in MAL's base
     url = 'https://myanimelist.net/animelist/{}/load.json?status=7&offset=0'.format(name)
-    response = requests.get(url)
+    response = REQ.get(url)
     if response.text == '{"errors":[{"message":"invalid request"}]}':
-      messagebox.showerror("Error!","No user found! Check spelling!")
+      TK.messagebox.showerror("Error!","No user found! Check spelling!")
       return
     data_json = response.json()
     #If first try went smooth offset increments to the high point where it's nearly impossible for users to have such big lists. When response returns error or hits max offset loop breaks.
     offset = 300
     while True:
       url = 'https://myanimelist.net/animelist/{}/load.json?status=7&offset={}'.format(name,offset)
-      response = requests.get(url)
+      response = REQ.get(url)
       if len(response.text) < 4: break
       data_json += response.json()
       offset += 300
@@ -102,27 +101,27 @@ def queryReqMAL(name):
     save = ET.ElementTree(root)
     save.write("listMAL.xml")
     grabFromFile("listMAL.xml")
-  if not name: messagebox.showerror("Name not given!","Input field is empty!")
+  if not name: TK.messagebox.showerror("Name not given!","Input field is empty!")
 
 #Tkinter window to communicate with user (yes everyone loves clickable interfaces). It has fixed window size.
 def main():
-  win = Tk()
+  win = TK.Tk()
   win.title("Random Anime Generator")
   win.geometry("600x400+660+340")
   win.resizable(False,False)
-  mainTitle = Label(win,text="Input your MAL/AL nickname below!",font=40,width=40)
+  mainTitle = TK.Label(win,text="Input your MAL/AL nickname below!",font=40,width=40)
   mainTitle.pack(pady=25)
-  userEntry = Entry(win,width=40)
+  userEntry = TK.Entry(win,width=40)
   userEntry.pack(pady=10)
-  buttonMAL = Button(win,text="Load MAL List",width=15,command=lambda:{queryReqMAL(userEntry.get()),userEntry.delete(0,"end")})
+  buttonMAL = TK.Button(win,text="Load MAL List",width=15,command=lambda:{queryReqMAL(userEntry.get()),userEntry.delete(0,"end")})
   buttonMAL.place(x=180,y=120)
-  buttonAL = Button(win,text="Load AL List",width=15,command=lambda:{queryReqAL(userEntry.get()),userEntry.delete(0,"end")})
+  buttonAL = TK.Button(win,text="Load AL List",width=15,command=lambda:{queryReqAL(userEntry.get()),userEntry.delete(0,"end")})
   buttonAL.place(x=305,y=120)
-  buttonListMAL = Button(win,text="Load Last MAL List",width=15,command=lambda:grabFromFile("listMAL.xml"))
+  buttonListMAL = TK.Button(win,text="Load Last MAL List",width=15,command=lambda:grabFromFile("listMAL.xml"))
   buttonListMAL.place(x=180,y=150)
-  buttonListAL = Button(win,text="Load Last AL List",width=15,command=lambda:grabFromFile("listAL.xml"))
+  buttonListAL = TK.Button(win,text="Load Last AL List",width=15,command=lambda:grabFromFile("listAL.xml"))
   buttonListAL.place(x=305,y=150)
-  warning = Label(win,text="DISCLAMER: Scrapping MAL list for the first time takes long time due to site's old API!",fg="red")
+  warning = TK.Label(win,text="DISCLAMER: Scrapping MAL list for the first time takes long time due to site's old API!",fg="red")
   warning.pack(pady=125)
   win.mainloop()
 
